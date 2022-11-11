6d9bf591e3b14a4028f086b88669da6a81a40094
an-seunghwan <dpeltms79@gmail.com>
2022-11-11 21:32:58 +0900

    Update model.py

--- modules/model.py
+++ modules/model.py
@@ -18,230 +18,78 @@ class VAE(nn.Module):
             nn.ReLU(),
             nn.Linear(16, 8),
             nn.ReLU(),
-            nn.Linear(8, config["node"] * 2),
+            nn.Linear(8, config["latent_dim"] * 2),
         ).to(device)
         
-        """decoder"""
-        self.decoder = nn.Sequential(
-            nn.Linear(config["node"], 300),
-            nn.ELU(),
-            nn.Linear(300, 300),
-            nn.ELU(),
-            nn.Linear(300, 3*config["image_size"]*config["image_size"]),
-            nn.Tanh()
+        """spline"""
+        # self.M = 10
+        self.delta = torch.arange(0, 1.1, step=0.1).view(1, -1)
+        self.M = self.delta.size(1)
+        self.spline = nn.Sequential(
+            nn.Linear(config["latent_dim"], 4),
+            nn.ReLU(),
+            nn.Linear(4, config["input_dim"] * (1 + self.M)),
         ).to(device)
     
     def get_posterior(self, input):
-        h = self.encoder(nn.Flatten()(input)) # [batch, node * 2]
-        mean, logvar = torch.split(h, self.config["node"], dim=1)
+        h = self.encoder(input)
+        mean, logvar = torch.split(h, self.config["latent_dim"], dim=1)
         return mean, logvar
     
-    def transform(self, input, log_determinant=False):
-        latent = torch.matmul(input, self.I_B_inv) # [batch, node], input = epsilon (exogenous variables)
-        orig_latent = latent.clone()
-        latent = torch.split(latent, 1, dim=1) # [batch, 1] x node
-        latent = list(map(lambda x, layer: layer(x, log_determinant=log_determinant), latent, self.flows)) # input = (I-B^T)^{-1} * epsilon
-        logdet = [x[1] for x in latent]
-        latent = [x[0] for x in latent]
-        return orig_latent, latent, logdet
-    
-    def encode(self, input, deterministic=False, log_determinant=False):
-        mean, logvar = self.get_posterior(input)
-        
-        """Latent Generating Process"""
+    def sampling(self, mean, logvar, deterministic=False):
         if deterministic:
-            epsilon = mean
+            z = mean
         else:
-            noise = torch.randn(input.size(0), self.config["node"]).to(self.device) 
-            epsilon = mean + torch.exp(logvar / 2) * noise
-        orig_latent, latent, logdet = self.transform(epsilon, log_determinant=log_determinant)
-        
-        return mean, logvar, epsilon, orig_latent, latent, logdet
+            noise = torch.randn(mean.size(0), self.config["latent_dim"]).to(self.device) 
+            z = mean + torch.exp(logvar / 2) * noise
+        return z
     
-    def forward(self, input, deterministic=False, log_determinant=False):
-        """encoding"""
-        mean, logvar, epsilon, orig_latent, latent, logdet = self.encode(input, 
-                                                                         deterministic=deterministic,
-                                                                         log_determinant=log_determinant)
+    def encode(self, input, deterministic=False):
+        mean, logvar = self.get_posterior(input)
+        z = self.sampling(mean, logvar, deterministic=deterministic)
+        return z, mean, logvar
+    
+    def quantile_parameter(self, z):
+        h = self.spline(z)
+        h = torch.split(h, 1 + self.M, dim=1)
         
-        """decoding"""
-        xhat = self.decoder(torch.cat(latent, dim=1))
-        xhat = xhat.view(-1, self.config["image_size"], self.config["image_size"], 3)
-        
-        """Alignment"""
-        _, _, _, _, align_latent, _ = self.encode(input, 
-                                                  deterministic=True, 
-                                                  log_determinant=log_determinant)
-        
-        return mean, logvar, epsilon, orig_latent, latent, logdet, align_latent, xhat
-#%%
-class Discriminator(nn.Module):
-    def __init__(self, config, device='cpu'):
-        super(Discriminator, self).__init__()
-        self.config = config
-        self.net = nn.Sequential(
-            nn.Linear(3*config["image_size"]*config["image_size"] + config["node"], 300),
-            nn.ELU(),
-            nn.Linear(300, 300),
-            nn.ELU(),
-            nn.Linear(300, 1),
-        ).to(device)
-
-    def forward(self, x, z):
-        x = x.view(-1, 3*self.config["image_size"]*self.config["image_size"])
-        x = torch.cat((x, z), dim=1)
-        return self.net(x)
+        gamma = [h_[:, [0]] for h_ in h]
+        beta = [nn.Softplus()(h_[:, 1:]) for h_ in h] # positive constraint
+        return gamma, beta
+    
+    def quantile_function(self, alpha, gamma, beta, j):
+        return gamma[j] + (beta[j] * torch.where(alpha - self.delta > 0,
+                                                alpha - self.delta,
+                                                torch.zeros(()))).sum(axis=1, keepdims=True)
+    
+    def forward(self, input, deterministic=False):
+        z, mean, logvar = self.encode(input, deterministic=deterministic)
+        gamma, beta = self.quantile_parameter(z)
+        return z, mean, logvar, gamma, beta
 #%%
 def main():
     #%%
     config = {
-        "image_size": 64,
-        "n": 10,
-        "node": 4,
-        "flow_num": 4,
-        "inverse_loop": 100,
-        "scm": 'linear',
-        "factor": [1, 1, 2],
+        "input_dim": 10,
+        "latent_dim": 2,
     }
     
-    B = torch.zeros(config["node"], config["node"])
-    B[:2, 2:] = 1
-    #%%
-    """CAD-VAE"""
-    mask = []
-    # light
-    m = torch.zeros(config["image_size"], config["image_size"], 3)
-    m[:20, ...] = 1
-    mask.append(m)
-    # angle
-    m = torch.zeros(config["image_size"], config["image_size"], 3)
-    m[20:51, ...] = 1
-    mask.append(m)
-    # shadow
-    m = torch.zeros(config["image_size"], config["image_size"], 3)
-    m[51:, ...] = 1
-    mask.append(m)
-    
-    model = GAM(B, mask, config, 'cpu')
+    model = VAE(config, 'cpu')
     for x in model.parameters():
         print(x.shape)
-    batch = torch.rand(config["n"], config["image_size"], config["image_size"], 3)
+    batch = torch.rand(10, config["input_dim"])
     
-    mean, logvar, epsilon, orig_latent, latent, logdet, align_latent, xhat_separated, xhat = model(batch)
+    z, mean, logvar, gamma, beta = model(batch)
     
-    inverse_diff = torch.abs(sum([x - y for x, y in zip(torch.split(orig_latent, 1, dim=1), 
-                                                        model.inverse(latent))]).sum())
-    assert inverse_diff / (config["n"] * config["node"]) < 1e-5
+    assert z.shape == (10, config["latent_dim"])
+    assert mean.shape == (10, config["latent_dim"])
+    assert logvar.shape == (10, config["latent_dim"])
+    assert gamma[0].shape == (10, 1)
+    assert len(gamma) == config["input_dim"]
+    assert beta[0].shape == (10, model.M)
+    assert len(beta) == config["input_dim"]
     
-    mean, logvar, epsilon, orig_latent, latent, logdet, align_latent, xhat_separated, xhat = model(batch)
-    mean, logvar, epsilon, orig_latent, latent, logdet, align_latent, xhat_separated, xhat = model(batch, log_determinant=True)
-    
-    assert mean.shape == (config["n"], config["node"])
-    assert logvar.shape == (config["n"], config["node"])
-    assert epsilon.shape == (config["n"], config["node"])
-    assert orig_latent.shape == (config["n"], config["node"])
-    assert latent[0].shape == (config["n"], 1)
-    assert len(latent) == config["node"]
-    assert logdet[0].shape == (config["n"], 1)
-    assert len(logdet) == config["node"]
-    assert align_latent[0].shape == (config["n"], 1)
-    assert len(align_latent) == config["node"]
-    assert xhat.shape == (config["n"], config["image_size"], config["image_size"], 3)
-    
-    # deterministic behavior
-    out1 = model(batch, deterministic=False)
-    out2 = model(batch, deterministic=True)
-    assert (out1[0] - out2[0]).abs().mean() == 0 # mean
-    assert (out1[1] - out2[1]).abs().mean() == 0 # logvar
-    assert (torch.cat(out1[4], dim=1) - torch.cat(out2[4], dim=1)).abs().mean() != 0 # latent
-    assert (torch.cat(out1[6], dim=1) - torch.cat(out2[6], dim=1)).abs().mean() == 0 # align_latent
-    
-    print("CAD-VAE pass test!")
-    print()
-    #%%
-    """Baseline VAE"""
-    model = VAE(B, config, 'cpu')
-    discriminator = Discriminator(config, 'cpu')
-    for x in model.parameters():
-        print(x.shape)
-    for x in discriminator.parameters():
-        print(x.shape)
-    batch = torch.rand(config["n"], config["image_size"], config["image_size"], 3)
-    
-    mean, logvar, epsilon, orig_latent, latent, logdet, align_latent, xhat = model(batch)
-    inverse_diff = torch.abs(sum([x - y for x, y in zip(torch.split(orig_latent, 1, dim=1), 
-                                                        model.inverse(latent))]).sum())
-    assert inverse_diff / (config["n"] * config["node"]) < 1e-5
-    
-    info = discriminator(batch, epsilon)
-    assert info.shape == (config["n"], 1)
-    
-    mean, logvar, epsilon, orig_latent, latent, logdet, align_latent, xhat = model(batch, log_determinant=True)
-    
-    assert mean.shape == (config["n"], config["node"])
-    assert logvar.shape == (config["n"], config["node"])
-    assert epsilon.shape == (config["n"], config["node"])
-    assert orig_latent.shape == (config["n"], config["node"])
-    assert latent[0].shape == (config["n"], 1)
-    assert len(latent) == config["node"]
-    assert logdet[0].shape == (config["n"], 1)
-    assert len(logdet) == config["node"]
-    assert align_latent[0].shape == (config["n"], 1)
-    assert len(align_latent) == config["node"]
-    assert xhat.shape == (config["n"], config["image_size"], config["image_size"], 3)
-    
-    # deterministic behavior
-    out1 = model(batch, deterministic=False)
-    out2 = model(batch, deterministic=True)
-    assert (out1[0] - out2[0]).abs().mean() == 0 # mean
-    assert (out1[1] - out2[1]).abs().mean() == 0 # logvar
-    assert (torch.cat(out1[4], dim=1) - torch.cat(out2[4], dim=1)).abs().mean() != 0 # latent
-    assert (torch.cat(out1[6], dim=1) - torch.cat(out2[6], dim=1)).abs().mean() == 0 # align_latent
-    
-    print("Baseline VAE pass test!")
-    print()
-    #%%
-    """Baseline Classifier"""
-    mask = []
-    # light
-    m = torch.zeros(config["image_size"], config["image_size"], 3)
-    m[:20, ...] = 1
-    mask.append(m)
-    # angle
-    m = torch.zeros(config["image_size"], config["image_size"], 3)
-    m[20:51, ...] = 1
-    mask.append(m)
-    # shadow
-    m = torch.zeros(config["image_size"], config["image_size"], 3)
-    m[51:, ...] = 1
-    mask.append(m)
-    m = torch.zeros(config["image_size"], config["image_size"], 3)
-    m[51:, ...] = 1
-    mask.append(m)
-    
-    model = Classifier(mask, config, 'cpu')
-    for x in model.parameters():
-        print(x.shape)
-    batch = torch.rand(config["n"], config["image_size"], config["image_size"], 3)
-    
-    pred = model(batch)
-    
-    assert pred.shape == (config["n"], config["node"])
-    
-    print("Baseline Classifier pass test!")
-    print()
-    #%%
-    """Downstream Classifier"""
-    model = DownstreamClassifier(config, 'cpu')
-    for x in model.parameters():
-        print(x.shape)
-    batch = torch.rand(config["n"], config["node"])
-    
-    pred = model(batch)
-    
-    assert pred.shape == (config["n"], 1)
-    
-    print("Downstream Classifier pass test!")
+    print("Model pass test!")
 #%%
 if __name__ == '__main__':
     main()
