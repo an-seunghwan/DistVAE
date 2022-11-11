ff82751251e221e7e408f44aa78280dcfd282d66
an-seunghwan <dpeltms79@gmail.com>
2022-11-11 21:40:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pandas as pd
 import tqdm
 from PIL import Image
 import matplotlib.pyplot as plt
-# plt.switch_backend('agg')
+plt.switch_backend('agg')
 
 import torch
 from torch import nn
@@ -23,23 +23,25 @@ from modules.simulation import (
 from modules.model import (
     VAE
 )
-#%%
-# import sys
-# import subprocess
-# try:
-#     import wandb
-# except:
-#     subprocess.check_call([sys.executable, "-m", "pip", "install", "wandb"])
-#     with open("./wandb_api.txt", "r") as f:
-#         key = f.readlines()
-#     subprocess.run(["wandb", "login"], input=key[0], encoding='utf-8')
-#     import wandb
 
-# run = wandb.init(
-#     project="VAE(CRPS)", 
-#     entity="anseunghwan",
-#     # tags=[""],
-# )
+from modules.train import train_VAE
+#%%
+import sys
+import subprocess
+try:
+    import wandb
+except:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "wandb"])
+    with open("./wandb_api.txt", "r") as f:
+        key = f.readlines()
+    subprocess.run(["wandb", "login"], input=key[0], encoding='utf-8')
+    import wandb
+
+run = wandb.init(
+    project="VAE(CRPS)", 
+    entity="anseunghwan",
+    tags=["Credit"],
+)
 #%%
 import argparse
 import ast
@@ -61,7 +63,7 @@ def get_args(debug):
     # optimization options
     parser.add_argument('--epochs', default=100, type=int,
                         help='maximum iteration')
-    parser.add_argument('--batch_size', default=128, type=int,
+    parser.add_argument('--batch_size', default=512, type=int,
                         help='batch size')
     parser.add_argument('--lr', default=0.001, type=float,
                         help='learning rate')
@@ -75,85 +77,6 @@ def get_args(debug):
     else:    
         return parser.parse_args()
 #%%
-def train_VAE(dataloader, model, config, optimizer, device):
-    logs = {
-        'loss': [], 
-        'quantile': [],
-        'KL': [],
-    }
-    # for debugging
-    for i in range(config["latent_dim"]):
-        logs['posterior_variance{}'.format(i+1)] = []
-    
-    for (x_batch, _) in tqdm.tqdm(iter(dataloader), desc="inner loop"):
-        
-        if config["cuda"]:
-            x_batch = x_batch.cuda()
-            # y_batch = y_batch.cuda()
-        
-        # with torch.autograd.set_detect_anomaly(True):    
-        optimizer.zero_grad()
-        
-        z, mean, logvar, gamma, beta = model(x_batch)
-        
-        loss_ = []
-        
-        """alpha_tilde"""
-        j = 0
-        alpha_tilde_list = []
-        for j in range(config["input_dim"]):
-            mask = [model.quantile_function(d, gamma, beta, j) for d in model.delta[0]]
-            mask = torch.cat(mask, axis=1)
-            mask = torch.where(mask <= x_batch[:, [j]], 
-                            mask, 
-                            torch.zeros(())).type(torch.bool).type(torch.float)
-            alpha_tilde = x_batch[:, [j]] - gamma[j]
-            alpha_tilde += (mask * beta[j] * model.delta).sum(axis=1, keepdims=True)
-            alpha_tilde /= (mask * beta[j]).sum(axis=1, keepdims=True) + 1e-6
-            alpha_tilde = torch.clip(alpha_tilde, 1e-4, 1) # numerical stability
-            alpha_tilde_list.append(alpha_tilde)
-        
-        """loss"""
-        j = 0
-        total_loss = 0
-        for j in range(config["input_dim"]):
-            term = (1 - model.delta.pow(3)) / 3 - model.delta - torch.maximum(alpha_tilde_list[j], model.delta).pow(2)
-            term += 2 * torch.maximum(alpha_tilde_list[j], model.delta) * model.delta
-            
-            loss = (2 * alpha_tilde_list[j]) * x_batch[:, [j]]
-            loss += (1 - 2 * alpha_tilde_list[j]) * gamma[j]
-            loss += (beta[j] * term).sum(axis=1, keepdims=True)
-            loss *= 0.5
-            total_loss += loss.mean()
-        # print(loss.mean())
-        loss_.append(('quantile', total_loss))
-        
-        """KL-Divergence"""
-        KL = torch.pow(mean, 2).sum(axis=1)
-        KL -= logvar.sum(axis=1)
-        KL += torch.exp(logvar).sum(axis=1)
-        KL -= config["latent_dim"]
-        KL *= 0.5
-        KL = KL.mean()
-        loss_.append(('KL', KL))
-        
-        ### posterior variance: for debugging
-        var_ = torch.exp(logvar).mean(axis=0)
-        for i in range(config["latent_dim"]):
-            loss_.append(('posterior_variance{}'.format(i+1), var_[i]))
-        
-        loss = total_loss + config["beta"] * KL 
-        loss_.append(('loss', loss))
-        
-        loss.backward()
-        optimizer.step()
-            
-        """accumulate losses"""
-        for x, y in loss_:
-            logs[x] = logs.get(x) + [y.item()]
-    
-    return logs
-#%%
 def main():
     #%%
     config = vars(get_args(debug=True)) # default configuration
@@ -166,12 +89,12 @@ def main():
     if config["cuda"]:
         torch.cuda.manual_seed(config["seed"])
     #%%
-    """
-    load dataset: Credit
-    Reference: https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud?resource=download
-    """
     class TabularDataset(Dataset): 
         def __init__(self, config):
+            """
+            load dataset: Credit
+            Reference: https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud?resource=download
+            """
             df = pd.read_csv('./data/creditcard.csv')
             df = df.sample(frac=1, random_state=config["seed"]).reset_index(drop=True)
             continuous = [x for x in df.columns if x != 'Class']
@@ -188,9 +111,10 @@ def main():
             self.mean = mean
             self.std = std
             train = (train - mean) / std
-            # test = (test - mean) / std
             self.train = train
             self.x_data = train.to_numpy()
+            
+            # test = (test - mean) / std
 
         def __len__(self): 
             return len(self.x_data)
@@ -213,7 +137,7 @@ def main():
         ax.flatten()[k].set_ylabel(v)
     plt.tight_layout()
     plt.savefig('./assets/empirical_quantile.png')
-    plt.show()
+    # plt.show()
     plt.close()
     #%%
     model = VAE(config, device).to(device)
@@ -232,34 +156,15 @@ def main():
         print_input += ''.join([', {}: {:.4f}'.format(x, np.mean(y)) for x, y in logs.items()])
         print(print_input)
         
-        # """update log"""
-        # wandb.log({x : np.mean(y) for x, y in logs.items()})
-            
-        # if epoch % 10 == 0:
-        #     plt.figure(figsize=(4, 4))
-        #     for i in range(9):
-        #         plt.subplot(3, 3, i+1)
-        #         plt.imshow((xhat[i].cpu().detach().numpy() + 1) / 2)
-        #         plt.axis('off')
-        #     plt.savefig('./assets/tmp_image_{}.png'.format(epoch))
-        #     plt.close()
-    
-    # """reconstruction result"""
-    # fig = plt.figure(figsize=(4, 4))
-    # for i in range(9):
-    #     plt.subplot(3, 3, i+1)
-    #     plt.imshow((xhat[i].cpu().detach().numpy() + 1) / 2)
-    #     plt.axis('off')
-    # plt.savefig('./assets/recon.png')
-    # plt.close()
-    # wandb.log({'reconstruction': wandb.Image(fig)})
+        """update log"""
+        wandb.log({x : np.mean(y) for x, y in logs.items()})
     #%%
     """model save"""
-    torch.save(model.state_dict(), './assets/model_{}_{}.pth'.format(config["model"], config["scm"]))
-    artifact = wandb.Artifact('model_{}_{}'.format(config["model"], config["scm"]), 
+    torch.save(model.state_dict(), './assets/model_credit.pth')
+    artifact = wandb.Artifact('model_credit', 
                             type='model',
                             metadata=config) # description=""
-    artifact.add_file('./assets/model_{}_{}.pth'.format(config["model"], config["scm"]))
+    artifact.add_file('./assets/model_credit.pth')
     artifact.add_file('./main.py')
     artifact.add_file('./modules/model.py')
     wandb.log_artifact(artifact)
