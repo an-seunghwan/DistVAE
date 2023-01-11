0da9aa9d57a0f0804ddf38099b2d0bb223a6c5f0
an-seunghwan <dpeltms79@gmail.com>
2023-01-11 21:42:09 +0900

    Create loan_datasets.py

---
+++ modules/loan_datasets.py
@@ -0,0 +1,92 @@
+#%%
+"""
+Data Source: https://www.kaggle.com/competitions/home-credit-default-risk/data?select=application_train.csv
+"""
+#%%
+import tqdm
+import os
+import numpy as np
+import pandas as pd
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import Dataset
+
+from collections import namedtuple
+
+OutputInfo = namedtuple('OutputInfo', ['dim', 'activation_fn'])
+#%%
+# https://chocoffee20.tistory.com/6
+class TabularDataset(Dataset): 
+    def __init__(self, train=True):
+        base = pd.read_csv('./data/application_train.csv')
+        base = base.sample(frac=1, random_state=0).reset_index(drop=True)
+        
+        self.continuous = [
+            'AMT_INCOME_TOTAL', 
+            'AMT_CREDIT', # target variable
+            'AMT_ANNUITY',
+            'AMT_GOODS_PRICE',
+            'REGION_POPULATION_RELATIVE', 
+            'DAYS_BIRTH', 
+            'DAYS_EMPLOYED', 
+            'DAYS_REGISTRATION',
+            'DAYS_ID_PUBLISH',
+            'OWN_CAR_AGE',
+        ]
+        self.discrete = [
+            'NAME_CONTRACT_TYPE',
+            'CODE_GENDER',
+            # 'FLAG_OWN_CAR',
+            'FLAG_OWN_REALTY',
+            'NAME_TYPE_SUITE',
+            'NAME_INCOME_TYPE',
+            'NAME_EDUCATION_TYPE',
+            'NAME_FAMILY_STATUS',
+            'NAME_HOUSING_TYPE',
+            'TARGET', # target variable
+        ]
+        base = base[self.continuous + self.discrete]
+        base = base.dropna()
+        base = base.iloc[:50000]
+        
+        # one-hot encoding
+        df_dummy = []
+        for d in self.discrete:
+            df_dummy.append(pd.get_dummies(base[d], prefix=d))
+        base = pd.concat([base.drop(columns=self.discrete)] + df_dummy, axis=1)
+        
+        if train:
+            df = base.iloc[:45000] # train
+            
+            df[self.continuous] = (df[self.continuous] - df[self.continuous].mean(axis=0))
+            df[self.continuous] /= df[self.continuous].std(axis=0)
+            
+            self.train = df
+            self.x_data = df.to_numpy()
+        else:
+            df_train = base.iloc[:45000] # train
+            df = base.iloc[45000:] # test
+            
+            df[self.continuous] = (df[self.continuous] - df_train[self.continuous].mean(axis=0))
+            df[self.continuous] /= df_train[self.continuous].std(axis=0)
+            
+            self.test = df
+            self.x_data = df.to_numpy()
+        
+        # Output Information
+        self.OutputInfo_list = []
+        for c in self.continuous:
+            self.OutputInfo_list.append(OutputInfo(1, 'CRPS'))
+        for d, dummy in zip(self.discrete, df_dummy):
+            self.OutputInfo_list.append(OutputInfo(dummy.shape[1], 'softmax'))
+        
+    def __len__(self): 
+        return len(self.x_data)
+
+    def __getitem__(self, idx): 
+        x = torch.FloatTensor(self.x_data[idx])
+        return x
+#%%
