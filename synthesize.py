9ade07f09febdc1d788394343182aaf21dc489a4
an-seunghwan <dpeltms79@gmail.com>
2023-01-23 22:00:23 +0900

    Update synthesize.py
    1. un-standardization of synthetic data using training mean, std
    2. standardization except for target variable

--- synthesize.py
+++ synthesize.py
@@ -180,23 +180,35 @@ def main():
     else:
         raise ValueError('Not supported dataset!')
     #%%
+    # un-standardization of synthetic data
+    ITS[dataset.continuous] = ITS[dataset.continuous] * dataset.std + dataset.mean
+    #%%
+    # standardization except for target variable
+    dataset.train[target] = dataset.train[target] * dataset.std[target] + dataset.mean[target]
+    test_dataset.test[target] = test_dataset.test[target] * dataset.std[target] + dataset.mean[target]
+    
+    cont = [x for x in dataset.continuous if x not in [target]]
+    ITS[cont] = (ITS[cont] - ITS[cont].mean(axis=0)) / ITS[cont].std(axis=0)
+    #%%
     # baseline
     print("\nBaseline: Machine Learning Utility in Regression...\n")
-    base_r2result = regression_eval(dataset.train, test_dataset.test, target)
-    wandb.log({'R^2 (Baseline)': np.mean([x[1] for x in base_r2result])})
+    base_reg = regression_eval(dataset.train, test_dataset.test, target)
+    wandb.log({'MAPE (Baseline)': np.mean([x[1] for x in base_reg])})
+    # wandb.log({'R^2 (Baseline)': np.mean([x[1] for x in base_reg])})
     #%%
     # Inverse Transform Sampling
     print("\nSynthetic: Machine Learning Utility in Regression...\n")
-    r2result = regression_eval(ITS, test_dataset.test, target)
-    wandb.log({'R^2 (ITS)': np.mean([x[1] for x in r2result])})
+    reg = regression_eval(ITS, test_dataset.test, target)
+    wandb.log({'MAPE (ITS)': np.mean([x[1] for x in reg])})
+    # wandb.log({'R^2 (ITS)': np.mean([x[1] for x in reg])})
     #%%
     # visualization
     fig = plt.figure(figsize=(5, 4))
-    plt.plot([x[1] for x in base_r2result], 'o--', label='baseline')
-    plt.plot([x[1] for x in r2result], 'o--', label='synthetic')
+    plt.plot([x[1] for x in base_reg], 'o--', label='baseline')
+    plt.plot([x[1] for x in reg], 'o--', label='synthetic')
     plt.ylim(0, 1)
     plt.ylabel('$R^2$', fontsize=13)
-    plt.xticks([0, 1, 2], [x[0] for x in base_r2result], fontsize=13)
+    plt.xticks([0, 1, 2], [x[0] for x in base_reg], fontsize=13)
     plt.legend()
     plt.tight_layout()
     plt.savefig('./assets/{}/{}_MLU_regression.png'.format(config["dataset"], config["dataset"]))
@@ -220,21 +232,21 @@ def main():
     #%%
     # baseline
     print("\nBaseline: Machine Learning Utility in Classification...\n")
-    base_f1result = classification_eval(dataset.train, test_dataset.test, target)
-    wandb.log({'F1 (Baseline)': np.mean([x[1] for x in base_f1result])})
+    base_clf = classification_eval(dataset.train, test_dataset.test, target)
+    wandb.log({'F1 (Baseline)': np.mean([x[1] for x in base_clf])})
     #%%
     # Inverse Transform Sampling
     print("\nSynthetic: Machine Learning Utility in Classification...\n")
-    f1result = classification_eval(ITS, test_dataset.test, target)
-    wandb.log({'F1 (ITS)': np.mean([x[1] for x in f1result])})
+    clf = classification_eval(ITS, test_dataset.test, target)
+    wandb.log({'F1 (ITS)': np.mean([x[1] for x in clf])})
     #%%
     # visualization
     fig = plt.figure(figsize=(5, 4))
-    plt.plot([x[1] for x in base_f1result], 'o--', label='baseline')
-    plt.plot([x[1] for x in f1result], 'o--', label='synthetic')
+    plt.plot([x[1] for x in base_clf], 'o--', label='baseline')
+    plt.plot([x[1] for x in clf], 'o--', label='synthetic')
     plt.ylim(0, 1)
     plt.ylabel('$F_1$', fontsize=13)
-    plt.xticks([0, 1, 2], [x[0] for x in base_f1result], fontsize=13)
+    plt.xticks([0, 1, 2], [x[0] for x in base_clf], fontsize=13)
     plt.legend()
     plt.tight_layout()
     plt.savefig('./assets/{}/{}_MLU_classification.png'.format(config["dataset"], config["dataset"]))
