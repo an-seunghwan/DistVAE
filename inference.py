ec4a3beed4e0efd6ce710d04dc1219077242b726
an-seunghwan <dpeltms79@gmail.com>
2023-01-31 20:16:26 +0900

    inference: empirical = step function

--- inference.py
+++ inference.py
@@ -18,6 +18,8 @@ from torch.utils.data import Dataset
 
 from modules.simulation import set_random_seed
 from modules.model import VAE
+
+from scipy import stats
 #%%
 import sys
 import subprocess
@@ -99,38 +101,6 @@ def main():
     
     model.eval()
     #%%    
-    """Empirical quantile plot"""
-    q = np.arange(0.01, 1, 0.01)
-    if config["dataset"] == "covtype":
-        fig, ax = plt.subplots(2, config["CRPS_dim"] // 2, 
-                               figsize=(3 * config["CRPS_dim"] // 2, 3 * 2))
-    elif config["dataset"] == "credit":
-        fig, ax = plt.subplots(2, config["CRPS_dim"] // 2, 
-                               figsize=(3 * config["CRPS_dim"] // 2, 3 * 2))
-    elif config["dataset"] == "loan":
-        fig, ax = plt.subplots(1, config["CRPS_dim"], 
-                               figsize=(3 * config["CRPS_dim"], 3 * 1))
-    elif config["dataset"] == "adult":
-        fig, ax = plt.subplots(1, config["CRPS_dim"], 
-                               figsize=(3 * config["CRPS_dim"], 3 * 1))
-    elif config["dataset"] == "cabs":
-        fig, ax = plt.subplots(1, config["CRPS_dim"], 
-                               figsize=(3 * config["CRPS_dim"], 3 * 1))
-    elif config["dataset"] == "kings":
-        fig, ax = plt.subplots(2, config["CRPS_dim"] // 2 + 1, 
-                               figsize=(3 * config["CRPS_dim"] // 2 + 1, 3 * 2))
-    else:
-        raise ValueError('Not supported dataset!')
-    
-    for k, v in enumerate(dataset.continuous):
-        ax.flatten()[k].plot(np.quantile(dataset.x_data[:, k], q=q), q)
-        ax.flatten()[k].set_xlabel(v)
-        ax.flatten()[k].set_ylabel('alpha')
-    plt.tight_layout()
-    plt.savefig('./assets/{}/{}_empirical_quantile.png'.format(config["dataset"], config["dataset"]))
-    # plt.show()
-    plt.close()
-    #%%
     """Quantile Estimation with sampling mechanism"""
     n = 100
     MC = 5000
@@ -152,31 +122,75 @@ def main():
     if config["dataset"] == "covtype":
         fig, ax = plt.subplots(2, config["CRPS_dim"] // 2, 
                                figsize=(3 * config["CRPS_dim"] // 2, 3 * 2))
+        integer = dataset.continuous
+        
     elif config["dataset"] == "credit":
         fig, ax = plt.subplots(2, config["CRPS_dim"] // 2, 
                                figsize=(3 * config["CRPS_dim"] // 2, 3 * 2))
+        integer = ['DAYS_BIRTH', 
+                'DAYS_EMPLOYED', 
+                'DAYS_ID_PUBLISH']
+        
     elif config["dataset"] == "loan":
         fig, ax = plt.subplots(1, config["CRPS_dim"], 
                                figsize=(3 * config["CRPS_dim"], 3 * 1))
+        integer = ['Age',
+                'Experience',
+                'Income', 
+                'CCAvg',
+                'Mortgage']
+        
     elif config["dataset"] == "adult":
         fig, ax = plt.subplots(1, config["CRPS_dim"], 
                                figsize=(3 * config["CRPS_dim"], 3 * 1))
+        integer = dataset.continuous
+        
     elif config["dataset"] == "cabs":
         fig, ax = plt.subplots(1, config["CRPS_dim"], 
                                figsize=(3 * config["CRPS_dim"], 3 * 1))
+        integer = ['Var2',
+                'Var3']
+        
     elif config["dataset"] == "kings":
         fig, ax = plt.subplots(2, config["CRPS_dim"] // 2 + 1, 
                                figsize=(3 * config["CRPS_dim"] // 2 + 1, 3 * 2))
+        integer = ['sqft_living',
+            'sqft_lot',
+            'sqft_above',
+            'sqft_basement',
+            'yr_built',
+            'yr_renovated',
+            'sqft_living15',
+            'sqft_lot15',]
+        
     else:
         raise ValueError('Not supported dataset!')
+    
+    orig = dataset.x_data[:, :len(dataset.continuous)] * np.array(dataset.std)
+    orig += np.array(dataset.mean)
+    orig = pd.DataFrame(orig, columns=dataset.continuous)[integer].astype(int)
+    
     for k, v in enumerate(dataset.continuous):
         ax.flatten()[k].plot(x_linspace[:, k], alpha_hat[:, k], label="sampled")
-        ax.flatten()[k].plot(np.quantile(dataset.x_data[:, k], q=q), q, label="empirical")
+        
+        x_linspace_orig = [np.arange(x, y, 1) for x, y in zip(
+            [np.quantile(orig.to_numpy()[:, k], q=0.01)],
+            [np.quantile(orig.to_numpy()[:, k], q=0.99)])][0]
+        if v in integer:
+            emp = [stats.percentileofscore(
+                orig.to_numpy()[:, dataset.continuous.index(v)],
+                x.item()) * 0.01 for x in x_linspace_orig]
+            ax.flatten()[k].step((x_linspace_orig - dataset.mean[k]) / dataset.std[k], 
+                                 emp, 
+                                 label="empirical")
+        else:
+            q = np.arange(0.01, 1, 0.01)
+            ax.flatten()[k].step(np.quantile(dataset.x_data[:, k], q=q), q, label="empirical")
         ax.flatten()[k].set_xlabel(v)
         ax.flatten()[k].set_ylabel('alpha')
     plt.legend()
     plt.tight_layout()
-    plt.savefig('./assets/{}/{}_sampling_estimated_quantile.png'.format(config["dataset"], config["dataset"]))
+    plt.savefig('./assets/{}/{}_estimated_quantile.png'.format(config["dataset"], config["dataset"]))
     # plt.show()
     plt.close()
     wandb.log({'Estimated quantile (sampling mechanism)': wandb.Image(fig)})
