faf92fce766698efa542d4766ae89967a734a48b
an-seunghwan <dpeltms79@gmail.com>
2023-02-07 14:20:50 +0900

    create shadow_data: generating shadow data

---
+++ shadow_data.py
@@ -0,0 +1,122 @@
+#%%
+import os
+# os.environ['KMP_DUPLICATE_LIB_OK']='True'
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+#%%
+import numpy as np
+import pandas as pd
+import tqdm
+from PIL import Image
+import matplotlib.pyplot as plt
+# plt.switch_backend('agg')
+
+import sys
+sys.path.append('../')
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import Dataset
+
+from modules.simulation import set_random_seed
+from modules.model import VAE
+#%%
+import sys
+import subprocess
+try:
+    import wandb
+except:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "wandb"])
+    with open("../wandb_api.txt", "r") as f:
+        key = f.readlines()
+    subprocess.run(["wandb", "login"], input=key[0], encoding='utf-8')
+    import wandb
+
+run = wandb.init(
+    project="DistVAE", 
+    entity="anseunghwan",
+    tags=['DistVAE', 'Privacy'],
+)
+#%%
+import argparse
+def get_args(debug):
+    parser = argparse.ArgumentParser('parameters')
+    
+    parser.add_argument('--num', type=int, default=0, 
+                        help='model version')
+    parser.add_argument('--dataset', type=str, default='covtype', 
+                        help='Dataset options: covtype, credit, loan, adult, cabs, kings')
+
+    if debug:
+        return parser.parse_args(args=[])
+    else:    
+        return parser.parse_args()
+#%%
+def main():
+    #%%
+    config = vars(get_args(debug=False)) # default configuration
+    
+    """model load"""
+    artifact = wandb.use_artifact('anseunghwan/DistVAE/DistVAE_{}:v{}'.format(config["dataset"], config["num"]), type='model')
+    for key, item in artifact.metadata.items():
+        config[key] = item
+    model_dir = artifact.download()
+    
+    if not os.path.exists('./privacy/{}'.format(config["dataset"])):
+        os.makedirs('./privacy/{}'.format(config["dataset"]))
+    
+    config["cuda"] = torch.cuda.is_available()
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+    wandb.config.update(config)
+    
+    set_random_seed(config["seed"])
+    torch.manual_seed(config["seed"])
+    if config["cuda"]:
+        torch.cuda.manual_seed(config["seed"])
+    #%%
+    import importlib
+    dataset_module = importlib.import_module('modules.{}_datasets'.format(config["dataset"]))
+    TabularDataset = dataset_module.TabularDataset
+    
+    dataset = TabularDataset()
+    test_dataset = TabularDataset(train=False)
+    dataloader = DataLoader(dataset, batch_size=config["batch_size"], shuffle=True)
+    test_dataloader = DataLoader(test_dataset, batch_size=config["batch_size"], shuffle=True)
+    
+    OutputInfo_list = dataset.OutputInfo_list
+    CRPS_dim = sum([x.dim for x in OutputInfo_list if x.activation_fn == 'CRPS'])
+    softmax_dim = sum([x.dim for x in OutputInfo_list if x.activation_fn == 'softmax'])
+    config["CRPS_dim"] = CRPS_dim
+    config["softmax_dim"] = softmax_dim
+    #%%
+    model = VAE(config, device).to(device)
+    if config["cuda"]:
+        model_name = [x for x in os.listdir(model_dir) if x.endswith('pth')][0]
+        model.load_state_dict(
+            torch.load(
+                model_dir + '/' + model_name))
+    else:
+        model_name = [x for x in os.listdir(model_dir) if x.endswith('pth')][0]
+        model.load_state_dict(
+            torch.load(
+                model_dir + '/' + model_name, map_location=torch.device('cpu')))
+    
+    model.eval()
+    #%%
+    """Inverse Transform Sampling"""
+    OutputInfo_list = dataset.OutputInfo_list
+    n = len(dataset.train)
+    m = len(test_dataset.test)
+    for s in tqdm.tqdm(range(10), desc="Generating shadow train and test datasets..."):
+        torch.manual_seed(s)
+        with torch.no_grad():
+            samples = model.generate_data(n + m, OutputInfo_list)
+        ITS = pd.DataFrame(samples.numpy(), columns=dataset.train.columns)
+        ITS.to_csv(f'./privacy/{config["dataset"]}/{config["num"]}_synthetic{s}.csv')
+    #%%
+    wandb.run.finish()
+#%%
+if __name__ == '__main__':
+    main()
+#%%
