d7a4f0300783cd98a4dd5f5eb59e0d2549803629
an-seunghwan <dpeltms79@gmail.com>
2023-02-22 20:42:04 +0900

    Update copula.py

--- copula/copula_modules/copula.py
+++ copula/copula_modules/copula.py
@@ -25,14 +25,14 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.1),
             nn.Linear(128, 128),
             nn.LeakyReLU(0.1),
-            nn.Linear(128, 128),
-            nn.LeakyReLU(0.1),
             nn.Linear(128, config["latent_dim"]),
             nn.LeakyReLU(0.1),
         ).to(device)
         
         self.mlp2 = nn.Sequential(
             nn.Linear(2 * config["latent_dim"], 32),
+            nn.LeakyReLU(0.1),
+            nn.Linear(32, 32),
             nn.LeakyReLU(0.1),
             nn.Linear(32, 32),
             nn.LeakyReLU(0.1),
