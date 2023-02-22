0827a6767d046011944dfc0068b935c41c2ecc57
an-seunghwan <dpeltms79@gmail.com>
2023-02-22 12:15:11 +0900

    copula module -> sampling update

--- copula/copula_modules/copula.py
+++ copula/copula_modules/copula.py
@@ -63,12 +63,19 @@ class NCECopula():
         return inv_cdf
     
     """Gibbs sampling"""
-    def gibbs_sampling(self, test_size, burn_in=100):
+    def gibbs_sampling(self, z, test_size, burn_in=100, grid_points=51):
         self.model.eval()
 
-        a = np.linspace(0, 1, self.config["grid_points"])
-        uv_samples = np.zeros((burn_in + test_size, self.config["data_dim"]))
-        uv_samples[0, :] = np.random.uniform(0, 1, self.config["data_dim"]) # random initialization
+        a = np.linspace(0, 1, grid_points)
+        samples = np.zeros((burn_in + test_size, self.config["data_dim"]))
+        samples[0, :] = np.random.uniform(0, 1, self.config["data_dim"]) # random initialization
+        
+        assert z.shape == (1, self.config["latent_dim"])
+        z = z.numpy()
+        z = np.repeat(
+            z, 
+            repeats=grid_points, 
+            axis=0).reshape(-1, self.config["latent_dim"]) # fixed
         
         for t in tqdm.tqdm(range(1, burn_in + test_size), desc="Gibbs Sampling..."):
             for i in range(self.config["data_dim"]):
@@ -77,8 +84,8 @@ class NCECopula():
                     # (t)th sample -> coordinate 0 : grid point approximate
                     uv_i_vector = np.concatenate(
                         (a.reshape(-1, 1), np.repeat(
-                            uv_samples[t-1, i+1:self.config["data_dim"]],
-                            repeats=self.config["grid_points"], axis=0).reshape(-1, 1)),axis=1)
+                            samples[[t-1], i+1 : self.config["data_dim"]],
+                            repeats=grid_points, axis=0).reshape(grid_points, -1)), axis=1)
                     
                 elif i > 0 and i < self.config["data_dim"]-1:
                     # (t-1)th sample -> coordinates k+1, ..., d-1 where k > 0
@@ -86,36 +93,39 @@ class NCECopula():
                     # (t)th sample -> coordinate k : grid point approximate
                     uv_i_vector_left = np.concatenate(
                         (np.repeat(
-                            uv_samples[t, 0:i],
-                            repeats=self.config["grid_points"], axis=0).reshape(-1, 1),
+                            samples[[t], 0:i],
+                            repeats=grid_points, axis=0).reshape(grid_points, -1),
                         a.reshape(-1, 1)), axis=1)
                     uv_i_vector = np.concatenate(
                         (uv_i_vector_left, 
                         np.repeat(
-                            uv_samples[t-1, i+1:self.config["data_dim"]],
-                            repeats=self.config["grid_points"], axis=0).reshape(-1, 1)), axis=1)
+                            samples[[t-1], i+1 : self.config["data_dim"]],
+                            repeats=grid_points, axis=0).reshape(grid_points, -1)), axis=1)
                     
                 else:
                     # (t-1)th sample -> coordinates 0, 1, ..., d-2
                     # (t)th sample -> coordinate d-1 : grid point approximate
                     uv_i_vector = np.concatenate(
                         (np.repeat(
-                            uv_samples[t, 0:i],
-                            repeats=self.config["grid_points"], axis=0).reshape(-1, 1),
+                            samples[[t], 0:i],
+                            repeats=grid_points, axis=0).reshape(grid_points, -1),
                         a.reshape(-1, 1)), axis=1)
                 
                 with torch.no_grad():
-                    h = self.model(torch.from_numpy(uv_i_vector).to(torch.float32))
-                conditional_density = h / (1 - h)
+                    h = self.model(
+                        torch.cat([
+                            torch.from_numpy(uv_i_vector).to(torch.float32),
+                            torch.from_numpy(z).to(torch.float32)], dim=1))
+                conditional_density = h / (1 - h + 1e-6) # numerical stability
                 conditional_density /= conditional_density.sum()
                 
                 icdf = self.inverse_transform_sampling(
                     conditional_density.numpy().squeeze(1),
-                    np.linspace(0, 1, self.config["grid_points"] + 1))
-                uv_samples[t, i] = icdf(np.random.uniform())
+                    np.linspace(0, 1, grid_points + 1))
+                samples[t, i] = icdf(np.random.uniform())
         
-        # burn-in period
-        uv_samples = uv_samples[burn_in:, ]
-        assert len(uv_samples) == test_size
-        return uv_samples
+        # # burn-in period
+        # samples = samples[burn_in:, ]
+        # assert len(samples) == test_size
+        return samples
 #%%
