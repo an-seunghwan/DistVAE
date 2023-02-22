af40eb2a15796a314ce627005f67991ea673d4bc
an-seunghwan <dpeltms79@gmail.com>
2023-02-22 11:10:47 +0900

    copula/train: original NCE (based on data)

--- copula/copula_modules/train.py
+++ copula/copula_modules/train.py
@@ -18,8 +18,7 @@ def train_copula(OutputInfo_list, dataloader, model, copula, config, device):
         
         with torch.no_grad():
             """pseudo-observations"""
-            z = torch.randn(x_batch.size(0), config["latent_dim"]).to(device)
-            gamma, beta, logit = model.quantile_parameter(z)
+            z, _, _, gamma, beta, logit = model(x_batch, deterministic=False)
             # continuous
             alpha_tilde_list = model.quantile_inverse(x_batch, gamma, beta)
             cont_pseudo = torch.cat(alpha_tilde_list, dim=1)
@@ -39,41 +38,18 @@ def train_copula(OutputInfo_list, dataloader, model, copula, config, device):
             disc_pseudo = torch.cat(disc_pseudo, dim=1)
             # all covariates
             pseudo = torch.cat([cont_pseudo, disc_pseudo], dim=1)
-            
-            """variational"""
-            pseudo_z, pseudo_mean, pseudo_logvar = model.encode(x_batch, deterministic=False)
         
         # Noise contrastive estimation
-        true = copula.model(
-            torch.cat([pseudo, pseudo_z], dim=1))
-        
-        prior = torch.distributions.MultivariateNormal(
-            loc=torch.zeros(1, config["latent_dim"]), 
-            covariance_matrix=torch.eye(config["latent_dim"]))
-        prior_density = prior.log_prob(pseudo_z).exp().unsqueeze(1)
-        
-        posterior_density = [torch.distributions.MultivariateNormal(
-            loc=mean, 
-            covariance_matrix=torch.diag(logvar.exp())
-            ).log_prob(z_).exp() 
-         for mean, logvar, z_ in zip(pseudo_mean, pseudo_logvar, pseudo_z)]
-        posterior_density = torch.vstack(posterior_density)
-        
         uniform = torch.rand(
-            pseudo.size(0), config["data_dim"]) # noise
-        noise = torch.zeros_like(true)
-        MC = 10
-        for _ in range(MC): # MC approximation
-            z = torch.randn(
-                pseudo.size(0), config["latent_dim"]).to(device) # prior
-            noise += copula.model(
-                torch.cat([uniform, z], dim=1))
-        noise /= MC
+            pseudo.size(0), config["data_dim"]).to(device) # noise
         
-        loss = - torch.log((true * prior_density) / (true * prior_density + posterior_density) + 1e-6)
-        loss += - torch.log(1 / (1 + noise) + 1e-6)
-        loss = loss.mean() # numerical stability
+        true = copula.model(
+            torch.cat([pseudo, z], dim=1))
+        noise = copula.model(
+            torch.cat([uniform, z], dim=1))
         
+        loss = - (torch.log(true + 1e-6) + torch.log(1 - noise + 1e-6)).mean() # numerical stability
+            
         loss.backward()
         copula.optimizer.step()
             
@@ -82,7 +58,7 @@ def train_copula(OutputInfo_list, dataloader, model, copula, config, device):
     
     return logs
 #%%
-# def train_copula(OutputInfo_list, dataloader, model, copula, config):
+# def train_copula(OutputInfo_list, dataloader, model, copula, config, device):
 #     logs = {
 #         'loss': [], 
 #     }
@@ -96,7 +72,8 @@ def train_copula(OutputInfo_list, dataloader, model, copula, config, device):
         
 #         with torch.no_grad():
 #             """pseudo-observations"""
-#             z, mean, logvar, gamma, beta, logit = model(x_batch, deterministic=False)
+#             z = torch.randn(x_batch.size(0), config["latent_dim"]).to(device)
+#             gamma, beta, logit = model.quantile_parameter(z)
 #             # continuous
 #             alpha_tilde_list = model.quantile_inverse(x_batch, gamma, beta)
 #             cont_pseudo = torch.cat(alpha_tilde_list, dim=1)
@@ -116,18 +93,41 @@ def train_copula(OutputInfo_list, dataloader, model, copula, config, device):
 #             disc_pseudo = torch.cat(disc_pseudo, dim=1)
 #             # all covariates
 #             pseudo = torch.cat([cont_pseudo, disc_pseudo], dim=1)
+            
+#             """variational"""
+#             pseudo_z, pseudo_mean, pseudo_logvar = model.encode(x_batch, deterministic=False)
         
 #         # Noise contrastive estimation
+#         true = copula.model(
+#             torch.cat([pseudo, pseudo_z], dim=1))
+        
+#         prior = torch.distributions.MultivariateNormal(
+#             loc=torch.zeros(1, config["latent_dim"]), 
+#             covariance_matrix=torch.eye(config["latent_dim"]))
+#         prior_density = prior.log_prob(pseudo_z).exp().unsqueeze(1)
+        
+#         posterior_density = [torch.distributions.MultivariateNormal(
+#             loc=mean, 
+#             covariance_matrix=torch.diag(logvar.exp())
+#             ).log_prob(z_).exp() 
+#          for mean, logvar, z_ in zip(pseudo_mean, pseudo_logvar, pseudo_z)]
+#         posterior_density = torch.vstack(posterior_density)
+        
 #         uniform = torch.rand(
 #             pseudo.size(0), config["data_dim"]) # noise
+#         noise = torch.zeros_like(true)
+#         MC = 10
+#         for _ in range(MC): # MC approximation
+#             z = torch.randn(
+#                 pseudo.size(0), config["latent_dim"]).to(device) # prior
+#             noise += copula.model(
+#                 torch.cat([uniform, z], dim=1))
+#         noise /= MC
         
-#         true = copula.model(
-#             torch.cat([pseudo, z], dim=1))
-#         noise = copula.model(
-#             torch.cat([uniform, z], dim=1))
+#         loss = - torch.log((true * prior_density) / (true * prior_density + posterior_density) + 1e-6)
+#         loss += - torch.log(1 / (1 + noise) + 1e-6)
+#         loss = loss.mean() # numerical stability
         
-#         loss = - (torch.log(true + 1e-6) + torch.log(1 - noise + 1e-6)).mean() # numerical stability
-            
 #         loss.backward()
 #         copula.optimizer.step()
             
